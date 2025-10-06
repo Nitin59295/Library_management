c78ce78f44f2f1f798245f181c476bfb4bbf624c
Nitin bhatia <134129893+Nitin59295@users.noreply.github.com>
2025-10-06 23:44:07 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,6 @@
-# accounts/admin.py
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your custom User model
+
 admin.site.register(User, UserAdmin)
